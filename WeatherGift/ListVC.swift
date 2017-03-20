35d2d11db4710be2e584e53ec729c9832107129e
Jake Mnich <jakemnich@Jakes-MacBook-Pro.local>
2017-03-20 15:12:15 -0400

    through 7.7

--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -11,6 +11,8 @@ import UIKit
 class ListVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
     
     var locationsArray = [String]()
     var currentPage = 0
@@ -36,6 +38,19 @@ class ListVC: UIViewController {
             controller.locationsArray = locationsArray
         }
     }
+    
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing == true {
+            tableView.setEditing(false, animated: true)
+            editBarButton.title = "Edit"
+            addBarButton.isEnabled = true
+        } else {
+            tableView.setEditing(true, animated: true)
+            editBarButton.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
+    
 }
 
 extension ListVC: UITableViewDataSource, UITableViewDelegate {
@@ -52,4 +67,49 @@ extension ListVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //We don't need to put any code here because teh cell triggers a segue
     }
+    
+    //MARK: - TableView editing functions
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            locationsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        //First make a copy of the item that you are going to move
+        let itemToMove = locationsArray[sourceIndexPath.row]
+        //delete item from original location
+        locationsArray.remove(at: sourceIndexPath.row)
+        //insert item into the "to" post move location
+        locationsArray.insert(itemToMove, at: destinationIndexPath.row)
+        
+    }
+    
+    //MARK: - TableView Code to freeze the first cell - No deleting or moving
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return (indexPath.row == 0 ? false : true)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return (indexPath.row == 0 ? false : true)
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
