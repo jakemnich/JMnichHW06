7262d3debc15beb217f98ac4fd705923952d4396
Jake Mnich <jakemnich@Jakes-MacBook-Pro.local>
2017-04-02 16:55:04 -0400

    Through 7.23

---
+++ WeatherGift/DayWeatherCell.swift
@@ -0,0 +1,42 @@
+//
+//  DayWeatherCell.swift
+//  WeatherGift
+//
+//  Created by Jake Mnich on 4/2/17.
+//  Copyright © 2017 Jake Mnich. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+
+    @IBOutlet weak var cellIcon: UIImageView!
+    @IBOutlet weak var cellWeekday: UILabel!
+    @IBOutlet weak var cellMaxTemp: UILabel!
+    @IBOutlet weak var cellMinTemp: UILabel!
+    @IBOutlet weak var cellSummary: UITextView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configureTableCell(dailyForecast: WeatherLocation.DailyForecast, timeZone: String) {
+        cellMaxTemp.text = String(format: "%2.f", dailyForecast.dailyMaxTemp) + "°"
+        cellMinTemp.text = String(format: "%2.f", dailyForecast.dailyMinTemp) + "°"
+        cellSummary.text = dailyForecast.dailySummary
+        cellIcon.image = UIImage(named: dailyForecast.dailyIcon)
+        let usableDate = Date(timeIntervalSince1970: dailyForecast.dailyDate)
+        let dateFormatter = DateFormatter()
+        let dailyTimeZone = TimeZone(identifier: timeZone)
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.timeZone = dailyTimeZone
+        let weekDay = dateFormatter.string(from: usableDate)
+        cellWeekday.text = weekDay
+    }
+
+}
